2f3366c735efd78acd9908b55a9f782b4c9259f1
walshm7 <95325262+walshm7@users.noreply.github.com>
2023-03-28 12:41:51 -0400

    Assignment Tracker Start
    Assignment Tracker Start

--- Flask/app.py
+++ Flask/app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from src.Models.models import *
 from src.Services.todo.todo import todo_page
 from src.Services.calendar import CalendarMonthly_page 
+from src.Services.assigments import assignment_page 
 
 app = Flask(__name__)
 #config file
@@ -13,6 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db.init_app(app)
 app.register_blueprint(todo_page)
 app.register_blueprint(CalendarMonthly_page)
+app.register_blueprint(assignment_page)
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     if request.method == "POST":
@@ -24,12 +26,13 @@ def index():
             return redirect('/')
         except:
             return "There was an issue adding your task"
+        
     else:
         tasks = Todo.query.order_by(desc(Todo.date_created)).all()
-        return render_template("dashboard.html", tasks=tasks)
         
-if __name__ ==  "__main__":
-    
-    app.run(debug = True) 
+        return render_template("dashboard.html", tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug = True)
     
 
